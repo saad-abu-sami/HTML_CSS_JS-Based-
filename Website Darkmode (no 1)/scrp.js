d21525133ff41c175ca7901d7a7f20571728d4d3
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-06-27 15:29:59 +0600

    Alhamdulillah

--- Website Darkmode (no 1)/scrp.js
+++ Website Darkmode (no 1)/scrp.js
@@ -4,3 +4,10 @@ let themeSwitch = document.querySelector('.themeSwitch');
          themeSwitch.classList.toggle('active');
          body.classList.toggle('dark');
       }
+
+let menutoggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+menutoggle.onclick = function() {
+   menutoggle.classList.toggle('active');
+   navigation.classList.toggle('active');
+}
