41ffe47cdc0013d4486cfdc150070eb248ebd6fa
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-07-11 22:17:58 +0600

    Alhamdulillah

--- Change Font-size (Part-1)/script_fc.js
+++ Change Font-size (Part-1)/script_fc.js
@@ -1,1 +1,9 @@
- 
+let buttons = document.querySelector('.buttons');
+let btn     = buttons.querySelectorAll('.btn');
+for (let i = 0; i < buttons.length; i++) {
+   btn[i].addEventListener('click', function(){
+      let current     = document.getElementsByClassName('active');
+      current[0].className = current[0].className.replace("active", "");
+      this.className += " active";
+   })
+}
