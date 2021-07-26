8f0dc8c10c2f1126a0d20db2a84fc90353e077ba
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-07-26 22:36:41 +0600

    Alhamdulillah

--- Split Video on Page Scroll (part-1)/script_SVPS.js
+++ Split Video on Page Scroll (part-1)/script_SVPS.js
@@ -1,1 +1,7 @@
-let a=10;
+let side1 = document.getElemenetById('side1');
+let side2 = document.getElemenetById('side2');
+
+window.addEventListener('scroll',function(){
+   side1.style.left = -window.pageYOffset+ 'px';
+   side2.style.left =  window.pageYOffset+ 'px';
+})
