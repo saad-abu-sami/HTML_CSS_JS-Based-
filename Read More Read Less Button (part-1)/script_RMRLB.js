e8efddb3d0b7d27a5a4c5f31ab8fbfd9f7ad428a
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-08-01 01:33:02 +0600

    Alhamdulillah

--- Read More Read Less Button (part-1)/script_RMRLB.js
+++ Read More Read Less Button (part-1)/script_RMRLB.js
@@ -1,1 +1,6 @@
-let a = 10
+let more = document.querySelectorAll('.more');
+for(let i = 0; i < more.length; i++){
+   more[i].addEventListener('click',function(){
+      more[i].parentNode.classList.toggle('active')
+   })
+}
