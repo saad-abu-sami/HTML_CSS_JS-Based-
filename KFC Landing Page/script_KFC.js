3a513a66d123cd7729e5e1e8f28c0db671c5a887
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-09-08 00:31:59 +0600

    boss

--- KFC Landing Page/script_KFC.js
+++ KFC Landing Page/script_KFC.js
@@ -1,1 +1,23 @@
-let kfc = 420;
+const slides = document.querySelectorAll('.slides');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+i = 0;
+
+function ActiveSlide(n){
+   for(slide of slides){
+      slide.classList.remove('active');
+      slides[n].classList.add('active');
+   }
+}
+//function for prev button
+prev.addEventListener('click', function(){
+   if(i == slides.length - 1){
+      i = 0;
+      ActiveSlide(n);
+   }
+   else{
+      i++;
+      ActiveSlide(i);
+   }
+})
