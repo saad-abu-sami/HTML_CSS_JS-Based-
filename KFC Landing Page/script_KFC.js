34fe79556080ac74fe278c9ab64e9e84b3ef3c55
Saad Abu Sami <60351152+saad-abu-sami@users.noreply.github.com>
2021-09-08 17:33:14 +0600

    Alhamdulillah END

--- KFC Landing Page/script_KFC.js
+++ KFC Landing Page/script_KFC.js
@@ -1,23 +1,31 @@
+//toggle
+const menutoggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+menutoggle.onclick = function () {
+   menutoggle.classList.toggle('active');
+   navigation.classList.toggle('active');
+};
+//slider
 const slides = document.querySelectorAll('.slides');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
 i = 0;
 
-function ActiveSlide(n){
-   for(slide of slides){
+function ActiveSlide(n) {
+   for (slide of slides) {
       slide.classList.remove('active');
       slides[n].classList.add('active');
    }
 }
 //function for prev button
-prev.addEventListener('click', function(){
-   if(i == slides.length - 1){
+prev.addEventListener('click', function () {
+   if (i == slides.length - 1) {
       i = 0;
       ActiveSlide(n);
    }
-   else{
+   else {
       i++;
       ActiveSlide(i);
    }
-})
+});
